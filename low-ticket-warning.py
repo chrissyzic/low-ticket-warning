b5eaf477490ce6997fe205285084a11a265b3c55
Chrissy Z <chrissy.ziccarelli@gmail.com>
2015-03-23 17:31:28 -0400

    Update low-ticket-warning.py

--- low-ticket-warning.py
+++ low-ticket-warning.py
@@ -4,6 +4,6 @@ tweets = ["@blackcatdc LOW TICKET ALERT: Big Data (3/27) and Twin Shadow (3/30).
 #Loop through each tweet in the list "tweets" and check for the phrase "lower ticket warning"
 for tweet in tweets:
 	if “low ticket warning” in tweet.lower() or "low ticket alert" in tweet.lower(): #make sure to make the tweet all lowercase because some venues tweet in all upper case, some use sentence case. By making them all lowercase we can make sure capital letters don't throw off the matching/conditional
-		print tweet #Eventually we can use some program to text us the tweet - that command will go on this line!
+		print tweet #Eventually we can use some program (Rapid SMS, maybe) to text us the tweet - that command will go on this line!
 	else:
 		print "No warnings - your tickets are safe for another day!"
